b0a3340fb094d0205b96c2157c0ca8174098967b
Dandi Lesmana <dandilesmana.if21@gmail.com>
2023-03-25 22:46:30 -0700

    add chirps(contorol,model,migrt), routes

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Chirp;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;   //returning  message 
+
+use Illuminate\View\View;      //call view
+
+class ChirpController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    // public function index(): Response 
+    // {
+    //     return response('Hello, World!');                   //test route and controller
+    // }
+
+    public function index(): View
+    {
+        return view('chirps.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Chirp $chirp)
+    {
+        //
+    }
+
+    /**S
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Chirp $chirp)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Chirp $chirp)
+    {
+        //
+    }
+}
